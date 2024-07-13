c0676256aebb5f20c622c95e7d9373e0cf4d4cb6
technoclem <technoclem.net@gmail.com>
2024-07-13 09:11:25 +0100

    Update

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CustomerManagementUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+
+        }
+    }
+}
